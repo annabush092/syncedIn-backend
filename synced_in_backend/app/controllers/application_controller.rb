750a0c424b75623727adfb86cf08ce1e5ae217a2
annabush092 <annabush092@gmail.com>
2017-12-03 17:19:16 -0500

    added awesome error handling in applicationcontroller

--- synced_in_backend/app/controllers/application_controller.rb
+++ synced_in_backend/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  rescue_from PG::ConnectionBad do |exception|
+    render json: {errors: ["Error connecting to the server. Check that your Postgres server is running."]}
+  end
+
 end
