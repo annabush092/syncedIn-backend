00f3960e1f4a8e0a97c3d87d0284d19438b9bf73
annabush092 <annabush092@gmail.com>
2017-12-05 18:24:46 -0500

    users can add instruments in edit page

--- synced_in_backend/app/controllers/api/v1/users_controller.rb
+++ synced_in_backend/app/controllers/api/v1/users_controller.rb
@@ -13,15 +13,10 @@ class Api::V1::UsersController < ApplicationController
   # end
 
   def create
-    @user = User.new(username: params[:username], password: params[:password], first_name: params[:first_name], last_name: params[:last_name])
-    if params[:instrument_ids]
-      @new_instruments = params[:instrument_ids].map do |t|
-        @user.association(:instruments).add_to_target(Instrument.find_by(id: t))
-      end
-    end
-    if params[:new_instrument]
+    @user = User.new(username: params[:username], password: params[:password], first_name: params[:first_name], last_name: params[:last_name], instrument_ids: params[:instrument_ids])
+    if params[:new_instrument].length > 0
       @instrument = Instrument.find_or_create_by(name: params[:new_instrument], family_id: params[:new_instrument_fam])
-      @user.association(:instruments).add_to_target(@instrument)
+      @user.instruments << @instrument
     end
     if @user.save
       token = new_token
@@ -33,13 +28,18 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     find_user
+    @user.assign_attributes(username: params[:username], first_name: params[:first_name], last_name: params[:last_name], instrument_ids: params[:instrument_ids])
+    if params[:new_instrument].length > 0
+      @instrument = Instrument.find_or_create_by(name: params[:new_instrument], family_id: params[:new_instrument_fam])
+      @user.instruments << @instrument
+    end
     auth_user = User.find_by(id: decode_user_id)
     if @user != auth_user
       render json: {errors: ["Invalid token. You must be logged in to edit your profile."]}
-    elsif @user.update(username: params[:username], first_name: params[:first_name], last_name: params[:last_name], instrument_ids: params[:instrument_ids])
+    elsif @user.save
       render json: @user
     else
-      render json: {errors: @user.errors.full_messages}
+      render json: {errors: @user.errors.full_messages.concat(@instrument.errors.full_messages)}
     end
   end
 
